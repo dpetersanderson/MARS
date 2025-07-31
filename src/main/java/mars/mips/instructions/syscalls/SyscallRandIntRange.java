199670399b15b2e0e5c3cf2164525d8939cfd5ab
Steven Lambeth <819106+ste-lam@users.noreply.github.com>
2025-07-31 19:54:30 +0200

    replaced "new Integer(n)" with "n"

--- src/main/java/mars/mips/instructions/syscalls/SyscallRandIntRange.java
+++ src/main/java/mars/mips/instructions/syscalls/SyscallRandIntRange.java
@@ -58,7 +58,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
           //    $a1 = the upper bound of range of returned values.
           // Return: $a0 = the next pseudorandom, uniformly distributed int value from this
           // random number generator's sequence.
-         Integer index = new Integer(RegisterFile.getValue(4));
+         Integer index = RegisterFile.getValue(4);
          Random stream = (Random) RandomStreams.randomStreams.get(index);
          if (stream == null) {
             stream = new Random(); // create a non-seeded stream
