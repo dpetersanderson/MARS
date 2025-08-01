a04aee301c6ca231af908cef1213a767c5688df8
Sa-RSt <sasha.rodelasteidle@gmail.com>
2025-08-01 00:06:24 -0300

    Usar subsistema sem console no Windows

--- wrappers/rust_wrapper/src/main.rs
+++ wrappers/rust_wrapper/src/main.rs
@@ -1,7 +1,18 @@
+#![windows_subsystem = "windows"]
+
 use std::{env, process};
 
 fn main() {
     let executable_path = env::current_exe().expect("Failed to get current executable path.");
-    let mut child = process::Command::new("java").arg("-jar").arg(executable_path).spawn().expect("Failed to spawn JVM.");
+    let java_executable = if env::consts::OS.contains("win") {
+        "javaw"
+    } else {
+        "java"
+    };
+    let mut child = process::Command::new(java_executable)
+        .arg("-jar")
+        .arg(executable_path)
+        .spawn()
+        .expect("Failed to spawn JVM.");
     child.wait().expect("Couldn't wait for child");
 }
