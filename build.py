cdced865be832f33244f075359392d81f08e6351
Sa-RSt <sasha.rodelasteidle@gmail.com>
2025-07-31 22:00:13 -0300

    Consertar build.py

--- build.py
+++ build.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     # create jar with resources and binaries
     binaries = glob('**/**.class', recursive=True)
-    exit_code = sp.call('jar cmf mainclass.txt Mars.jar PseudoOps.txt Config.properties Syscall.properties Settings.properties MARSlicense.txt mainclass.txt MipsXRayOpcode.xml registerDatapath.xml controlDatapath.xml ALUcontrolDatapath.xml Mars.java Mars.class docs help images mars', shell=True)
+    exit_code = sp.call('jar cmf mainclass.txt Mars.jar PseudoOps.txt Config.properties Syscall.properties Settings.properties MARSlicense.txt mainclass.txt MipsXRayOpcode.xml registerDatapath.xml controlDatapath.xml ALUcontrolDatapath.xml Mars.java Mars.class help images mars', shell=True)
     print(f'\njar returned: {exit_code}')
     try:
         shutil.copy('Mars.jar', '/mnt/Mars.jar')
